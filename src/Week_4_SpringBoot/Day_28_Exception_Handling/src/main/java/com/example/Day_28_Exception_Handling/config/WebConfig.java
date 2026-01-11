987d1321b9ab9739af09c45a8d85512d0a559607
Krishnakant Purohit <kmpurohit2000@gmail.com>
2026-01-11 18:42:30 +0530

    Update CORS configuration to allow additional origin for deployment

--- src/Week_4_SpringBoot/Day_28_Exception_Handling/src/main/java/com/example/Day_28_Exception_Handling/config/WebConfig.java
+++ src/Week_4_SpringBoot/Day_28_Exception_Handling/src/main/java/com/example/Day_28_Exception_Handling/config/WebConfig.java
@@ -10,7 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("http://localhost:5173") // Allow React
+                .allowedOrigins("http://localhost:5173",
+                        "https://student-management-system-roan-tau.vercel.app"
+                ) 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow these actions
                 .allowedHeaders("*")
                 .allowCredentials(true);
